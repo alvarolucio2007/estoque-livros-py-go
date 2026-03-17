dd0941039cb9272ff6cf1baf45269527e9a8b980
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-03-17 11:30:09 -0300

    Começando testes de API.

--- src/testing/rotasApi_test.go
+++ src/testing/rotasApi_test.go
@@ -1,27 +1,46 @@
 package test
 
-//func TestAPI(t *testing.T) {
-//	t.Run("apiListarLivros", func(t *testing.T) {
-//		testarHandlerCarregarLivros(t)
-//	})
-//}
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-//func testarHandlerCarregarLivros(t *testing.T) {
-//	gin.SetMode(gin.TestMode)
-//	r := gin.Default()
-//	r.GET("/livros", HandlerListarLivros)
-//	criarLivroTeste(t)
-//	url := "/livros"
-//	req, _ := http.NewRequest("GET", url, nil)
-//	w := httptest.NewRecorder()
-//	r.ServeHTTP(w, req)
-//	if w.Code != http.StatusOK {
-//		t.Errorf("esperava status 200,recebi %d", w.Code)
-//	}
-//}
+	"github.com/alvarolucio2007/estoque-livros-py/src/internal/handlers"
+	"github.com/gin-gonic/gin"
+)
 
-//func testarHandlerListarID(t *testing.T) {
-//	gin.SetMode(gin.TestMode)
-//	r := gin.Default()
-//	r.GET("/livros/listar_id", testarHandlerListarID)
-//}
+func TestAPI(t *testing.T) {
+	t.Run("apiListarLivros", func(t *testing.T) {
+		testarHandlerCarregarLivros(t)
+	})
+}
+
+func setupRouter() *gin.Engine {
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+	r.GET("/livros", handlers.HandlerListarLivros)
+	r.GET("/livros/listar_id", handlers.HandlerListarID)
+	r.GET("/livros/relatorio", handlers.HandlerListarRelatorio)
+	r.GET("/livros/:livro_id", handlers.HandlerBuscarID)
+	r.GET("/livros/titulo/:titulo", handlers.HandlerBuscarTitulo)
+	r.GET("/livros/autor/:autor", handlers.HandlerBuscarAutor)
+	r.POST("/livros", handlers.HandlerCadastrarLivro)
+	r.PUT("/livros/:id", handlers.HandlerAtualizarLivro)
+	r.DELETE("/livros/:id", handlers.HandlerDeletarLivro)
+	return r
+}
+
+func TestHandlerListarLivros(t *testing.T) {
+	r := setupRouter()
+	criarLivroTeste(t)
+	req, _ := http.NewRequest("GET", "/livros", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("Esperava 200, recebi %d", w.Code)
+	}
+}
+func TestHandlerCadastrarLivros(t *testing.T){
+	r:=setupRouter()
+	body := `{"titulo": "O Senhor dos Anéis", "autor": "Tolkien", "preco": 59.90, "ano": 1954, "quantidade": 10}`
+}
