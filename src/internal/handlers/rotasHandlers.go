a2ad4f4affb8d2765d385fc42ee4efd5bd0e1425
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-03-17 07:30:37 -0300

    Terminando os Handlers. TODO: Colocar os Handlers nas APIs, e ajustar os erros nos outros arquivos para se tornarem mais métricos.

--- src/internal/handlers/rotasHandlers.go
+++ src/internal/handlers/rotasHandlers.go
@@ -84,7 +84,7 @@ func HandlerBuscarID(c *gin.Context) {
 	c.JSON(http.StatusOK, resultado)
 }
 
-func HandelerBuscarTitulo(c *gin.Context) {
+func HandlerBuscarTitulo(c *gin.Context) {
 	tituloStr := c.Param("titulo")
 	resultado, err := verifiers.ServicoBuscarLivroTitulo(tituloStr)
 	if err != nil {
@@ -92,4 +92,63 @@ func HandelerBuscarTitulo(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, resultado)
+}
+
+func HandlerBuscarAutor(c *gin.Context) {
+	autorStr := c.Param("autor")
+	resultado, err := verifiers.ServicoBuscarLivroAutor(autorStr)
+	if err != nil {
+		SendError(c, err)
+		return
+	}
+	c.JSON(http.StatusOK, resultado)
+}
+
+func HandlerCadastrarLivro(c *gin.Context) {
+	var novoLivro models.LivroCadastrar
+	if err := c.ShouldBindJSON(&novoLivro); err != nil {
+		SendError(c, err)
+		return
+	}
+	err := verifiers.ServicoAdicionarLivro(novoLivro.Titulo, novoLivro.Autor, novoLivro.Preco, novoLivro.Ano, novoLivro.Quantidade)
+	if err != nil {
+		SendError(c, err)
+		return
+	}
+	c.JSON(http.StatusCreated, "criado com sucesso")
+}
+
+func HandlerAtualizarLivro(c *gin.Context) {
+	idStr := c.Param("id")
+	idUint, err := strconv.ParseUint(idStr, 10, 64)
+	if err != nil {
+		SendError(c, models.ErrIDNulo)
+		return
+	}
+	var dadosAtualizados models.Livro
+	if err := c.ShouldBindJSON(&dadosAtualizados); err != nil {
+		SendError(c, err)
+		return
+	}
+	err = verifiers.ServicoAtualizarLivro(uint(idUint), dadosAtualizados)
+	if err != nil {
+		SendError(c, err)
+		return
+	}
+	c.JSON(http.StatusAccepted, dadosAtualizados)
+}
+
+func HandlerDeletarLivro(c *gin.Context) {
+	idStr := c.Param("id")
+	idUint, err := strconv.ParseUint(idStr, 10, 64)
+	if err != nil {
+		SendError(c, models.ErrIDNulo)
+		return
+	}
+	err = verifiers.ServicoDeletarLivro(uint(idUint))
+	if err != nil {
+		SendError(c, err)
+		return
+	}
+	c.JSON(http.StatusAccepted, "livro deletado com sucesso")
 }
