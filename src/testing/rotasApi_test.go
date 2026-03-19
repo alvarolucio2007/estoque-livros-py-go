466ad2d2564bac5f6a8a89c41748f56509d1d92f
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-03-19 11:24:50 -0300

    Finalizando testes de Rotas API! TODO: Fazer funcionar agora no Streamlit, e dockeirizar.

--- src/testing/rotasApi_test.go
+++ src/testing/rotasApi_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 
@@ -24,6 +25,18 @@ func TestAPI(t *testing.T) {
 	})
 	t.Run("apiBuscarID", func(t *testing.T) {
 		TestHandlerBuscarID(t)
+	})
+	t.Run("apiBuscarTitulo", func(t *testing.T) {
+		TestHandlerBuscarTitulo(t)
+	})
+	t.Run("apiBuscarAutor", func(t *testing.T) {
+		TestHandlerBuscarAutor(t)
+	})
+	t.Run("apiAtualizarLivro", func(t *testing.T) {
+		TestHandlerAtualizarLivro(t)
+	})
+	t.Run("apiDeletarLivro", func(t *testing.T) {
+		TestHandlerDeletarLivro(t)
 	})
 }
 
@@ -67,7 +80,7 @@ func setupRouter() *gin.Engine {
 
 func TestHandlerListarLivros(t *testing.T) {
 	r := setupRouter()
-	criarLivroTeste(t)
+	criarLivroTesteAux(t)
 	req, _ := http.NewRequest("GET", "/livros", nil)
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
@@ -109,6 +122,46 @@ func TestHandlerBuscarID(t *testing.T) {
 	}
 }
 
+func TestHandlerBuscarTitulo(t *testing.T) {
+	r := setupRouter()
+	livro := helpers.CriarLivroTesteAux(t)
+	tituloEscapado := url.PathEscape(livro.Titulo)
+	url := fmt.Sprintf("/livros/titulo/%s", tituloEscapado)
+	req, _ := http.NewRequest("GET", url, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	ChecarCodigo(t, w.Code, http.StatusOK)
+	var resultado []models.Livro
+	err := json.Unmarshal(w.Body.Bytes(), &resultado)
+	if err != nil {
+		t.Fatalf("Erro ao decodificar JSON: %v", err)
+	}
+	if len(resultado) == 0 {
+		t.Fatal("esperava pelo menos um livro no resultado, mas veio vazio")
+	}
+	if resultado[0].Autor != livro.Autor {
+		t.Errorf("Esperava autor %s, mas veio %s", livro.Autor, resultado[0].Autor)
+	}
+}
+
+func TestHandlerBuscarAutor(t *testing.T) {
+	r := setupRouter()
+	livro := helpers.CriarLivroTesteAux(t)
+	autorEscapado := url.PathEscape(livro.Autor)
+	urlBusca := fmt.Sprintf("/livros/autor/%s", autorEscapado)
+	req, _ := http.NewRequest("GET", urlBusca, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	ChecarCodigo(t, w.Code, http.StatusOK)
+	var resultados []models.Livro
+	if err := json.Unmarshal(w.Body.Bytes(), &resultados); err != nil {
+		t.Fatalf("erro no unmarshal: %v", err)
+	}
+	if len(resultados) == 0 || resultados[0].Titulo != livro.Titulo {
+		t.Errorf("busca falhou,esperava titulo %s", livro.Titulo)
+	}
+}
+
 func TestHandlerCadastrarLivros(t *testing.T) {
 	r := setupRouter()
 	body := `{"titulo": "O Senhor dos Anéis", "autor": "Tolkien", "preco": 59.90, "ano": 1954, "quantidade": 10}`
@@ -117,4 +170,38 @@ func TestHandlerCadastrarLivros(t *testing.T) {
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	ChecarCodigo(t, w.Code, http.StatusCreated)
+}
+
+func TestHandlerAtualizarLivro(t *testing.T) {
+	r := setupRouter()
+	body := `{"titulo": "O Senhor dos Anéis", "autor": "Tolkien", "preco": 59.90, "ano": 1954, "quantidade": 10}`
+	livroCriado := helpers.CriarLivroTesteAux(t)
+	url := fmt.Sprintf("/livros/%d", livroCriado.ID)
+	req, _ := http.NewRequest("PUT", url, strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	ChecarCodigo(t, w.Code, http.StatusAccepted)
+	atualizado, _ := database.BuscarPorID(livroCriado.ID)
+	if atualizado.Titulo != "O Senhor dos Anéis" {
+		t.Errorf("O título não foi atualizado! Recebido: %s", atualizado.Titulo)
+	}
+}
+
+func TestHandlerDeletarLivro(t *testing.T) {
+	r := setupRouter()
+	livro := helpers.CriarLivroTesteAux(t)
+	url := fmt.Sprintf("/livros/%d", livro.ID)
+	req, _ := http.NewRequest("DELETE", url, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	ChecarCodigo(t, w.Code, http.StatusOK)
+
+	atualizado, err := database.BuscarPorID(livro.ID)
+	if atualizado != nil {
+		t.Errorf("livro não deletado!")
+	}
+	if err == nil {
+		t.Error("esperava um erro de não encontrado, mas não veio erro.")
+	}
 }
